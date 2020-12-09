8c167ee1275d661bc3b6ca3e8da929b6d43ebefc
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-12-09 18:46:17 -0500

    :heavy_plus_sign: create pageable.

---
+++ js/pageable.js
@@ -0,0 +1,28 @@
+function createPageable (list = [], page = 0, elementsPerPage = 0) {
+    let index = 0
+    let totalElements = list.length
+    // find total pages
+    let totalPages = Math.floor(totalElements / elementsPerPage)
+    if(totalElements % elementsPerPage > 0) {
+        totalPages ++
+    }
+    // retornable
+    let pageable = {
+        elements: [],
+        totalPages: 0,
+        elementsPerPage: 0,
+        currentPage: 0,
+    }
+    // calcule index
+    index = (elementsPerPage * page)
+    // get page elements
+    let elements = list.slice( index, ( index + elementsPerPage))
+
+    // fill pageable
+    pageable.elements = elements
+    pageable.totalPages = totalPages
+    pageable.elementsPerPage = elementsPerPage
+    pageable.currentPage = page
+
+    return pageable
+}
