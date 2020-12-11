c383baebda8bf1d0e0a0cbb4b6c981afceaca681
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-12-11 14:31:48 -0500

    :pencil: Add more data.

--- js/data.js
+++ js/data.js
@@ -121,10 +121,210 @@ const list = [
   },
   {
     "id": 13,
-    "name": "Ring of Fire",
+    "name": "Ophion's Violent Wind",
     "item_category": {
       "id": 4,
-      "name": "Rings",
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 14,
+    "name": "Dria's Robust",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 15,
+    "name": "Perth's Rune Sphinx",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 16,
+    "name": "Metis's Slayer Ancient",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 17,
+    "name": "Hegaton's Dragon",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 18,
+    "name": "Castol's Guardian",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 19,
+    "name": "Taros's Legendary",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 20,
+    "name": "Amis's Storm Crow",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 21,
+    "name": "Meter's Adamantine",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 22,
+    "name": "Nemesis's Red Wing",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 23,
+    "name": "Trite's Storm Jahad",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 24,
+    "name": "Deruff's Gru Hill",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 25,
+    "name": "Peihu's Rune Legendary",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 26,
+    "name": "Ceres's Slayer Demonic",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 27,
+    "name": "Momus's Great Dragon",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 28,
+    "name": "Thanatos's Dark Soul",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 29,
+    "name": "Hera's Red Spirit",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 30,
+    "name": "Eos's Thunder Hawk",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 31,
+    "name": "Uranus's Dark Master",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 32,
+    "name": "Nyx's Demonic",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
+      "state": true
+    },
+    "state": true
+  },
+  {
+    "id": 33,
+    "name": "Titan's Piercing Groove",
+    "item_category": {
+      "id": 4,
+      "name": "Set Lucky",
       "state": true
     },
     "state": true
